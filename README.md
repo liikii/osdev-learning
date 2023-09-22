c8efbc334b5362ffff83938ee71fe536ec0756f0
liikii <609957891@qq.com>
2023-09-22 22:54:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,112 @@
+## os-dev learning
+code clone from https://github.com/szhou42/osdev
+---
+
+# edu OS
+An operating system based on <a href = "http://www.osdever.net/bkerndev/Docs/title.htm"> Brans' Kernel Development Tutorial</a>  
+<a href = "https://www.youtube.com/watch?v=846d4m0wHQo"> Click me to see the video demo! </a>  
+
+Future goals:
+1. Userspace, Port Newlib, Multi-process, multi-thread, IPC, write syscalls, etc(process related stuff)
+2. Implement a hobby level TCP/IP stack(Ethernet/ARP/DHCP/IP/UDP/TCP with congestion control and everything..)
+3. Graphical User Interface(C/S GUI Server, Composite via IPC, now the GUI is in kernel)
+4. Write three GUI user apps(Shell, TCP ChatRoom, Text Editor)
+
+# Screenshots  
+![Alt text](/os_screenshots/ss15.png?raw=true "ss15")
+![Alt text](/os_screenshots/ss0.png?raw=true "ss0")
+![Alt text](/os_screenshots/ss1.png?raw=true "ss1")
+
+
+
+
+# How to run and test edu OS kernel
+## Preparation(Ubuntu14.04 is a recommended environment for compiling and running edu os)
+eduos use elf-i686-gcc as cross-compiler, please build the cross compiler on your system first.  
+Remember to replace "CC=i686-pc-btos-gcc" in Makefile with your cross compiler.
+
+<a href = "http://wiki.osdev.org/GCC_Cross-Compiler">How to build a cross compiler</a>  
+Or, a rewritten/translated version of the above wiki article in Simplified Chinese  
+
+Link: <a href = "http://pan.baidu.com/s/1hsg6AEg">环境搭建与交叉编译器</a> Download password: 7ksb  
+
+Then, install nasm 
+```
+sudo apt-get install nasm
+```
+
+Run the following script to generate 4 hard disk image
+```
+./mkext2image.sh
+```
+
+Run the following script to setup the tap network device
+```
+./setup_tap_device.sh
+```
+
+Note: The path of executable qemu-bridge-helper may vary, and so you should manually replace this path in qemu_run.sh, kvm.sh and debug_qemu.sh first. 
+Use this command to find the path of qemu-bridge-helper 
+```
+cd ~
+find . -iname qemu-bridge-helper
+```
+
+If you would like to run the os in GUI mode, set the constant GUI_MODE (defined in kmain.c) to 1. 
+If you would like to run the os in Network mode, set the constant NETWORK_MODE (defined in kmain.c) to 1. 
+
+Now run the following script to compile the kernel
+```
+./compile.sh
+```
+
+And then choose one of the following simulator to boot edu os. QEMU is more recommended.
+
+## QEMU
+1.  Run sudo apt-get install qemu-system
+2.  Run ./qemu_run.sh to boot edu os(or ./kvm.sh if your environment supports kvm)
+
+
+### Debug with QEMU
+1. run ./debug_qemu.sh
+
+
+# Plan
+#### Mem management  
+&#160; &#160; &#160; &#160;0 Higher half loader                [✔]  
+&#160; &#160; &#160; &#160;1 physical memory management        [✔]  
+&#160; &#160; &#160; &#160;2 paging(virtual memory management) [✔]  
+&#160; &#160; &#160; &#160;3 kmalloc                           [✔]  
+
+#### Filesystem  
+&#160; &#160; &#160; &#160;0 PCI Enumerate,R/W     [✔]  
+&#160; &#160; &#160; &#160;1 ATA/DMA Driver        [✔]  
+&#160; &#160; &#160; &#160;2 Ext2 Filesystem       [✔]  
+&#160; &#160; &#160; &#160;3 Virtual Filesystem    [✔]  
+
+#### Graphical Interface  
+&#160; &#160; &#160; &#160;0 vesa driver, 1024 * 768 color(32-bit color)，framebuffer mode                                              [✔]  
+&#160; &#160; &#160; &#160;1 Milestone(try loading and showing a wallpaper from hardisk!)                                               [✔]  
+&#160; &#160; &#160; &#160;2 Windows compositor, support fonts, buttons,and etc                                                         [✔]  
+&#160; &#160; &#160; &#160;3 Write a shell                                                                                              []  
+&#160; &#160; &#160; &#160;4 Write a file browser                                                                                       []  
+
+#### Miscellaneous  
+&#160; &#160; &#160; &#160;1 Usermode                                                               [✔]  
+&#160; &#160; &#160; &#160;1 Multi-tasking/Simple scheduler                                         []  
+&#160; &#160; &#160; &#160;2 Executable loader(elf)                                                 [✔]  
+&#160; &#160; &#160; &#160;3 spinlock, mutex, and other pthread primitives                          []  
+&#160; &#160; &#160; &#160;4 Add more syscalls, like fork/exec, open, read, etc..                   []  
+&#160; &#160; &#160; &#160;5 Port newlib                                                            []  
+&#160; &#160; &#160; &#160;6 Write or port some baisc utilities such asenv, rm, cp, mkdir, reboot   []  
+&#160; &#160; &#160; &#160;7 Real time clock                                                        [✔]  
+&#160; &#160; &#160; &#160;9 stadard input/output stuff                                             []  
+
+#### Network protocols  
+&#160; &#160; &#160; &#160;0 Send/Recv raw packets  [✔]  
+&#160; &#160; &#160; &#160;1 IP                     [✔]  
+&#160; &#160; &#160; &#160;2 UDP                    [✔]  
+&#160; &#160; &#160; &#160;3 ARP                    [✔]  
+&#160; &#160; &#160; &#160;4 DHCP                   [✔]  
+&#160; &#160; &#160; &#160;5 TCP                    []  
+&#160; &#160; &#160; &#160;6 HTTP                   []  
---
+++ src/kernel/kmain.c
@@ -0,0 +1,229 @@
+#include <multiboot.h>
+#include <system.h>
+#include <printf.h>
+#include <gdt.h>
+#include <idt.h>
+#include <tss.h>
+#include <vga.h>
+#include <timer.h>
+#include <asciiart.h>
+#include <pmm.h>
+#include <paging.h>
+#include <kheap.h>
+#include <pci.h>
+#include <ata.h>
+#include <vfs.h>
+#include <string.h>
+#include <ext2.h>
+#include <process.h>
+#include <usermode.h>
+#include <syscall.h>
+#include <elf_loader.h>
+#include <vesa.h>
+#include <bitmap.h>
+#include <compositor.h>
+#include <mouse.h>
+#include <keyboard.h>
+#include <font.h>
+#include <rtc.h>
+#include <rtl8139.h>
+#include <ethernet.h>
+#include <ip.h>
+#include <udp.h>
+#include <dhcp.h>
+#include <serial.h>
+#include <blend.h>
+#include <spinlock.h>
+
+
+extern uint8_t * bitmap;
+extern ata_dev_t primary_master;
+extern datetime_t current_datetime;
+
+#define MSIZE 48 * M
+#define GUI_MODE 0
+#define NETWORK_MODE 0
+
+void user_process2() {
+    uint32_t lock = 0;
+    while(1) {
+        for(int i = 0; i < 10000; i++) {
+            for(int j= 0; j < 2000; j++) {
+
+            }
+        }
+        spinlock_lock(&lock);
+        qemu_printf("hi there2\n");
+        spinlock_unlock(&lock);
+    }
+}
+
+void user_process() {
+    uint32_t lock = 0;
+    for(int i = 0; i < 20; i++) {
+        create_process_from_routine(user_process2, "user process2");
+    }
+    while(1) {
+        for(int i = 0; i < 10000; i++) {
+            for(int j= 0; j < 2000; j++) {
+
+            }
+        }
+        spinlock_lock(&lock);
+        qemu_printf("hi there\n");
+        spinlock_unlock(&lock);
+    }
+}
+
+
+int kmain(multiboot_info_t * mb_info) {
+    completed_init();
+
+    process_init();
+    syscall_init();
+
+    // Set TSS stack so that when process return from usermode to kernel mode, the kernel have a ready-to-use stack
+    uint32_t esp;
+    asm volatile("mov %%esp, %0" : "=r"(esp));
+    tss_set_stack(0x10, esp);
+
+    // Start the first process
+    create_process_from_routine(user_process, "user process");
+
+    qemu_printf("\nDone!\n");
+    for(;;);
+    return 0;
+}
+
+
+/*
+ * Move all the init code to here, so the kmain function is more clean and easier to read
+ * */
+void completed_init() {
+    // 清 vga内存区
+    video_init();
+    qemu_printf("%s\n", simpleos_logo);
+
+    // Initialize everything (green)
+    set_curr_color(LIGHT_GREEN);
+    qemu_printf("Initializing video(text mode 80 * 25)...\n");
+
+    qemu_printf("Initializing gdt, idt and tss...\n");
+    // kernel code, kernel data, user code, user data, tss seg
+    gdt_init();
+
+    // init idt
+    idt_init();
+    tss_init(5, 0x10, 0);
+
+    qemu_printf("Initializing physical memory manager...\n");
+    // bitmap init
+    pmm_init(1096 * M);
+
+    qemu_printf("Initializing paging...\n");
+    paging_init();
+
+    // 在提供的代码库中，kheap_init函数的目的是初始化操作系统的堆内存管理器。它接受三个参数：start、end和max。
+
+    // start参数表示堆内存的起始地址，即可用内存块的开始位置。
+    // end参数表示堆内存的结束地址，即可用内存块的结束位置。
+    // max参数表示堆内存的最大地址，即堆可以扩展的最大位置。
+
+    // 通过这些参数的设置，kheap_init函数将为堆内存管理器设置初始值，以确保堆管理器可以正确地分配和释放内存。这样，操作系统就能够使用堆内存来动态分配和管理内存，以满足程序的运行需求。
+    qemu_printf("Initializing kernel heap...\n");
+    kheap_init(KHEAP_START, KHEAP_START + KHEAP_INITIAL_SIZE, KHEAP_MAX_ADDRESS);
+
+    qemu_printf("Initializing timer...\n");
+    timer_init();
+
+    qemu_printf("Initializing pci...\n");
+    pci_init();
+
+    qemu_printf("Initializing keyboard...\n");
+    keyboard_init();
+
+    qemu_printf("Initializing vfs, ext2 and ata/dma...\n");
+    vfs_init();
+    ata_init();
+    ext2_init("/dev/hda", "/");
+
+
+    qemu_printf("Initializing real time clock...\n");
+    rtc_init();
+    qemu_printf("Current date and time: %s\n", datetime_to_str(&current_datetime));
+
+#if GUI_MODE
+    vesa_init();
+    compositor_init();
+    mouse_init();
+
+    // Terminal
+    window_t * red_w = window_create(get_super_window(), 20, 300, 750, 450, WINDOW_NORMAL, "window_black");
+    window_add_title_bar(red_w);
+    window_add_close_button(red_w);
+    window_add_minimize_button(red_w);
+    window_add_maximize_button(red_w);
+    canvas_t canvas_red = canvas_create(red_w->width, red_w->height, red_w->frame_buffer);
+    set_font_color(VESA_COLOR_BLACK + 1);
+    draw_text(&canvas_red, "Terminal", 1, 42);
+    window_add_round_corner(red_w);
+    blend_windows(red_w);
+
+    // File Browser
+    window_t * green_w = window_create(get_super_window(), 100, 100, 400, 400, WINDOW_NORMAL, "window_classic");
+    window_add_title_bar(green_w);
+    qemu_printf("Adding close buttons\n");
+    window_add_close_button(green_w);
+    qemu_printf("Adding minimize buttons\n");
+    window_add_minimize_button(green_w);
+    qemu_printf("Adding maximize buttons\n");
+    window_add_maximize_button(green_w);
+    canvas_t canvas_green = canvas_create(green_w->width, green_w->height, green_w->frame_buffer);
+    set_font_color(VESA_COLOR_BLACK + 1);
+    draw_text(&canvas_green, "File browser", 1, 19);
+    window_add_round_corner(green_w);
+    blend_windows(green_w);
+
+    // Top desktop bar
+    window_t * bar_w = window_create(get_super_window(), 0, 0, 1024, 25, WINDOW_DESKTOP_BAR, "desktop_bar");
+    canvas_t canvas_bar = canvas_create(bar_w->width, bar_w->height, bar_w->frame_buffer);
+    set_font_color(VESA_COLOR_BLACK+1);
+    draw_text(&canvas_bar, get_current_datetime_str(), 1, 115);
+    blend_windows(bar_w);
+    display_all_window();
+    video_memory_update(NULL, 0);
+    print_windows_depth();
+#endif
+
+#if NETWORK_MODE
+    qemu_printf("Initializing network driver...\n");
+    rtl8139_init();
+    arp_init();
+
+    uint8_t mac_addr[6];
+    mac_addr[0] = 0xAA;
+    mac_addr[1] = 0xBB;
+    mac_addr[2] = 0xCC;
+    mac_addr[3] = 0xDD;
+    mac_addr[4] = 0xEE;
+    mac_addr[5] = 0xFF;
+    get_mac_addr(mac_addr);
+
+    uint8_t ip_addr[6];
+    ip_addr[0] = 10;
+    ip_addr[1] = 0;
+    ip_addr[2] = 2;
+    ip_addr[3] = 15;
+    char * str = "This is a message sent from simpleos to an Ubuntu host";
+    //ethernet_send_packet(mac_addr, str, strlen(str), 0x0021);
+    //ip_send_packet(ip_addr, str, strlen(str));
+
+    // Ask QEMU's dhcp server for an IP address
+    dhcp_discover();
+    // If IP is ready, send a UDP message from simpleos to a host machine running Ubuntu
+    while(gethostaddr(mac_addr) == 0);
+    udp_send_packet(ip_addr, 1234, 1153, str, strlen(str));
+    for(;;);
+#endif
+
+}
